122646cf1accf956eb605ceb5a3ca78a621e1239
Julie Gumerman <julie.gumerman@gmail.com>
2019-09-30 13:45:41 -0600

    can now get data and locate one piece of data by id

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ server.use(express.json());
 
 ///res/req functions go here!!!
 
+server.get("/", (req, res) => {
+    res.send("Find your true North!!!");
+});
+
+server.get("/data", (req, res) => {
+    dataModel.find()
+        .then(data => {res.send(data)})
+        .catch(err => {res.send(err)})
+})
+
+
+
+server.get("/data/:id", (req, res) => {
+    const id = req.params.id;
+    dataModel.findById(id)
+        .then(data => {res.send(data)})
+        .catch(err => {res.send(err)})
+}) 
+
+
+
 const port = 8000;
 
 server.listen(port, () => console.log("Server up!!!"));
