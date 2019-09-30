d6e94174e8a0eaebb2004fb07ba3d51da68c0002
Julie Gumerman <julie.gumerman@gmail.com>
2019-09-30 14:35:07 -0600

    attempts at error messages

--- index.js
+++ index.js
@@ -26,18 +26,37 @@ server.get("/data/:id", (req, res) => {
     const id = req.params.id;
     dataModel.findById(id)
         .then(data => {res.send(data)})
-        .catch(err => {res.send(err)})
+        .catch(err => {res.status(500).send(`{error: "The hobbit's info could not be retrieved"}`)})
 }) 
 
 server.post("/data", (req, res) => {
     const addData = req.body;
     dataModel.insert(addData)
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
+        .then(data => res.status(201).json(data))
+        .catch(err => res.status(400).json(`{errorMessage: "Please provide name and bio for the hobbit."}`))
     
 })
 
 
+server.put("/data/:id", (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    dataModel.update(id, changes)
+        .then(hobbit => {
+            res.json(hobbit);
+            console.log(hobbit);
+        })
+        .catch(err => res.json(err));
+})
+
+
+
+server.delete("/data/:id", (req, res) => {
+    const id = req.params.id;
+    dataModel.remove(id)
+        .then(hobbit => {res.json(hobbit)})
+        .catch(err => {res.json(err)})
+})
 
 const port = 8000;
 
