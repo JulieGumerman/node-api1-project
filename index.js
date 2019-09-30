1087350f299203d3e155a2783e8725cdc2887f5f
Julie Gumerman <julie.gumerman@gmail.com>
2019-09-30 13:32:29 -0600

    server established

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+
+const express = require("express");
+
+const  dataModel = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+///res/req functions go here!!!
+
+const port = 8000;
+
+server.listen(port, () => console.log("Server up!!!"));
